6e293b5752f2d3255a8fe38581dd4e866528b7f3
BeckettPorter <porterbeckett@gmail.com>
2025-02-27 14:46:15 -0800

    redid everything

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 /**
  * Plagiarism Checker
  * A tool for finding the longest shared substring between two documents.
@@ -5,8 +7,8 @@
  * @author Zach Blick
  * @author YOUR NAME HERE
  */
-public class PlagiarismChecker {
-
+public class PlagiarismChecker
+{
     /**
      * This method finds the longest sequence of characters that appear in both texts in the same order,
      * although not necessarily contiguously.
@@ -18,6 +20,48 @@ public class PlagiarismChecker {
 
         // TODO Complete this function to return the length of the longest shared substring.
 
-        return 0;
+        int[][] lengthsAr = new int[doc1.length()][doc2.length()];
+
+        // Given every char, go through rest of strings and find possible other combos
+
+
+        for (int x = 0; x < doc1.length(); x++)
+        {
+            for (int y = 0; y < doc2.length(); y++)
+            {
+                int char1 = doc1.charAt(x);
+                int char2 = doc2.charAt(y);
+
+                if (char1 == char2)
+                {
+                    if (isArLocInBounds(x - 1, y - 1, lengthsAr))
+                    {
+                        lengthsAr[x][y] = lengthsAr[x - 1][y - 1] + 1;
+                    }
+                }
+                else
+                {
+                    if (isArLocInBounds(x - 1, y - 1, lengthsAr))
+                    {
+                        lengthsAr[x][y] = lengthsAr[x - 1][y - 1];
+                    }
+                }
+            }
+        }
+
+        return lengthsAr[lengthsAr.length - 1][lengthsAr[0].length - 1];
+    }
+
+    private static boolean isArLocInBounds(int x, int y, int[][] ar)
+    {
+        if (x < 0 || x >= ar.length)
+        {
+            return false;
+        }
+        if (y < 0 || y >= ar[0].length)
+        {
+            return false;
+        }
+        return true;
     }
 }
